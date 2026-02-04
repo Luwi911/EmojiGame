5dc46973370d038faf6837c6d128af3b4fedf32c
Tessa <tessaroberts@cmail.carleton.ca>
2026-02-04 15:22:08 -0500

    Defined colours

--- Main.py
+++ Main.py
@@ -8,3 +8,9 @@ FPS = 60 #--> Smoother movement
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Pygame Emoji Game Jam")
 clock = pygame.time.Clock()
+
+#Defining colours 
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+BLUE = (0, 0, 255)
