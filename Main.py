fe4b6930767f25ebebe2c5cdee16fba65dff9ffa
Tessa <tessaroberts@cmail.carleton.ca>
2026-02-04 17:00:19 -0500

    emoji collision counter working & image rects created

--- Main.py
+++ Main.py
@@ -1,4 +1,7 @@
 import pygame
+import sys
+import os
+pygame.font.init() #--> imports the timer font
 
 pygame.init()
 
@@ -17,22 +20,33 @@ BLUE = (0, 0, 255)
 
 #Defining Objects
 bg_image = pygame.image.load("testbg.webp")
-player_image = pygame.image.load("Steve.png")
+player_image = pygame.image.load("emoji1.png")
 
-# Load and scale background image
+emoji1_visible = True
+emoji2_visible = True
+
+# Load and scale background image & reload the background
 background = pygame.image.load("testbg.webp") #--> loads an image instead of a rectangle
 background = pygame.transform.scale(background, (WIDTH, HEIGHT)) #--> Refreshes the background
 
 # player image
 player_image = pygame.image.load("Steve.png")
 
-# emoji image
+# emoji images
 emoji1_image = pygame.image.load("emoji1.png")
+emoji2_image = pygame.image.load("emoji2.png")
 
 # player location and speed
 player_x = WIDTH // 2
 player_y = HEIGHT // 2
 player_speed = 4
+
+#Creating object rects
+emoji_rect = pygame.Rect(WIDTH // 2, HEIGHT // 2, 30, 30)
+
+# Incrementing the score counter
+score = 0
+score_increment = 1
 
 #movement variables (NEW)
 player_dx = 0 #--> "difference in X" (starts @ 0 bc it's not moving at first)
@@ -74,11 +88,35 @@ while not end:
     player_x += player_dx 
     player_y += player_dy
 
+    p_rec = pygame.Rect(player_x, player_y, player_image.get_width(),player_image.get_height())
+    e1_rec = pygame.Rect(180,40, emoji1_image.get_width(),emoji1_image.get_height())
+    e2_rec = pygame.Rect(425,147, emoji2_image.get_width(),emoji2_image.get_height())
+    #Score updating - emoji 1
+    if p_rec.colliderect(e1_rec) and emoji1_visible==True:
+        emoji1_visible = False
+        score += score_increment
+    
+
+#make a rectangle for our images w/ same width and height and X ,Y
+
+
+    if p_rec.colliderect(e2_rec) and emoji2_visible==True:
+        emoji2_visible = False
+        score += score_increment
+
 
     screen.blit(background, (0, 0))  # draw background Image ("Block Image Transfer" = blit)
     # B. draw player
     screen.blit(player_image, (player_x, player_y))  # draw player
-    screen.blit(emoji1_image, (0,0))
+    if emoji1_visible == True:
+        screen.blit(emoji1_image, (180,40))
+    if emoji2_visible == True:
+        screen.blit(emoji2_image, (425, 147))
+
+    #Score font to be visible
+    font = pygame.font.Font(None, 36)
+    score_text = font.render(f"Score: {score}", True, WHITE)
+    screen.blit(score_text, (10, 10))
 
     pygame.display.flip()
     clock.tick(FPS)
