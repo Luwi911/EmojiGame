e6e814c35ea406da552e017472e30b80c72ee2fa
Tessa <tessaroberts@cmail.carleton.ca>
2026-02-04 15:18:54 -0500

    Added more screen variables

--- Main.py
+++ Main.py
@@ -4,3 +4,7 @@ pygame.init()
 
 #Estalish screen WIDTH & HEIGHT
 WIDTH, HEIGHT = (500, 400)
+FPS = 60 #--> Smoother movement
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Pygame Emoji Game Jam")
+clock = pygame.time.Clock()
